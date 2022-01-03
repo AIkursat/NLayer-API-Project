68f20077f7b9f57f7a71d7601dd1f01dfd63c1c4
AIKursat <aynurkursat1999@hotmail.com>
2022-01-03 17:38:44 +0300

    Finish the UnitOfWork

---
+++ NLayerProject.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using NLayerProject.Core.Repositories;
+using NLayerProject.Core.UnitOfWorks;
+using NLayerProject.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Data.UnitOfWorks
+{
+    class UnitOfWork : IUnitOfWork
+    {
+
+        private readonly AppDbContext _context;
+
+        private ProductRepository _productRepository;
+        private CategoryRepository _categoryRepository;
+
+        public IProductRepository Products => _productRepository = _productRepository ?? new ProductRepository(_context); // If it's null, create new one and assign that.
+
+        public ICategoryRepository Categories => _categoryRepository = _categoryRepository ?? new CategoryRepository(_context);
+
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public void Commit() // Save Changes.
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
