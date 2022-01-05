5b0674b2db7d0db8031afb79e183c46c3064a091
AIKursat <aynurkursat1999@hotmail.com>
2022-01-05 21:20:11 +0300

    Finish the Person's action.

---
+++ NLayerProject.API/Controllers/PersonsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayerProject.Core.Entities;
+using NLayerProject.Core.Service;
+using System.Threading.Tasks;
+
+namespace NLayerProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private readonly IService<Person> _PersonService;
+
+        public PersonsController(IService<Person> PersonService)
+        {
+            _PersonService = PersonService;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetAll()
+        {
+            var persons = await _PersonService.GetAllAsync();
+            return Ok(persons);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Save(Person person)
+        {
+            var newperson = await _PersonService.AddAsync(person);
+            return Ok(newperson);
+
+        }
+
+
+    }
+}
--- NLayerProject.Data/AppDbContext.cs
+++ NLayerProject.Data/AppDbContext.cs
@@ -28,7 +28,6 @@ namespace NLayerProject.Data
         
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
-
         public DbSet<Person> Persons { get; set; }
 
 
