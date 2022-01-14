57141c003c9203bfeacfc678a6af0ab3f061c709
AIKursat <aynurkursat1999@hotmail.com>
2022-01-14 16:48:15 +0300

    Delete the Web Class Libarary

--- NLayerProject.Web/DTOs/ProductWithCategoryDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NLayerProject.Web.DTOs
-{
-    public class ProductWithCategoryDto : ProductDto
-    {
-        public CategoryDto Category { get; set; } // Must be same like we wrote Core layer.
-    }
-}
